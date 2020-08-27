dc353d38987a6ba370f96604f1f284add5e7d485
maanefisk <62015771+maanefisk@users.noreply.github.com>
2020-08-27 14:20:26 +0200

    Initial commit

---
+++ Oppgave06_Tilfeldige_Firkanter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oppgave06_Tilfeldige_Firkanter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
